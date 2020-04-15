3ee0957d2ff6f317d730504ba3c5b7a74459f82e
songlin <songlin.li@outlook.com>
2020-04-15 20:52:31 +0800

    删除ak

--- src/test/java/cn/wuxia/common/test/OssMainTest.java
+++ src/test/java/cn/wuxia/common/test/OssMainTest.java
@@ -12,15 +12,5 @@ import java.util.Map;
 public class OssMainTest {
     public static void main(String[] args) {
 
-        OSSUtils ossUtils = new OSSUtils("oss-cn-shenzhen.aliyuncs.com","LTAIBvTZ87DQZZrR", "whpaRiBfVj0YmhhbhglJNBBQM553bL");
-       Map map = ossUtils.putObject("img-www", "def/","efg.png", new File("/Users/songlin/Desktop/tongguheren.png"));
-
-        System.out.println(map);
-        String abc = EncodeUtils.urlDecode(ossUtils.client.generatePresignedUrl("img-www", "def/efg.png", DateUtil.addDays(new Date(), 1)).toString());
-        System.out.println(abc);
-//      map =  ossUtils.getObjectByParams("img-www", map.get("key").toString());
-//        System.out.println(map);
-//        OSSObject ossObject = (OSSObject) map.get("object");
-//        System.out.println(ossObject.getKey());
     }
 }
